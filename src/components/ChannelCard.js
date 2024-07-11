03872e69683fe2ec04078db556bbe2f1ef599251
shivakumar2006 <official.shivakumar06@gmail.com>
2024-07-11 15:08:57 +0530

    update

--- src/components/ChannelCard.js
+++ src/components/ChannelCard.js
@@ -21,7 +21,27 @@ const ChannelCard = ({ channelDetail }) => {
             textAlign: "center",
             color: "#fff",
           }}
-        ></CardContent>
+        >
+          <CardMedia
+            image={
+              channelDetail?.snippet?.thumbnails?.hign?.url ||
+              demoProfilePicture
+            }
+            alt={channelDetail?.snippet?.title}
+            sx={{
+              borderRadius: "50%",
+              height: "180px",
+              width: "180px",
+              mb: 2,
+              border: "1px solid #e3e3e3",
+            }}
+          />
+
+          <Typography variant="h6">
+            {channelDetail?.snippet?.title}
+            <CheckCircle sx={{ fontSize: 14, color: "gray", ml: "5px" }} />
+          </Typography>
+        </CardContent>
       </Link>
     </Box>
   );
