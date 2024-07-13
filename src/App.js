8ad23c1ba81c1b659f7ec4682694f7eba30ef45e
shivakumar2006 <official.shivakumar06@gmail.com>
2024-07-13 11:40:25 +0530

    add som echanges

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter, Router, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { Box } from "@mui/material";
 
 import {
--- src/components/VideoCard.js
+++ src/components/VideoCard.js
@@ -1,9 +1,8 @@
 import { Link } from "react-router-dom";
 import { Typography, Card, CardContent, CardMedia } from "@mui/material";
-import { CheckCircles, CheckCircle } from "@mui/icons-material";
+import { CheckCircle } from "@mui/icons-material";
 
 import {
-  demoThumbnailUrl,
   demoVideoUrl,
   demoVideoTitle,
   demoChannelUrl,
